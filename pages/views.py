e88663251da5096a5435630e16c68aca46660913
Yonas Negash <yonas.negash91@gmail.com>
2019-08-28 17:09:01 +0300

    render index using django block

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 def index(request):
-    return HttpResponse('<h1>Worked!</h1>')
+    return render(request, 'pages/index.html')
