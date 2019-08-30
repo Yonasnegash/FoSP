29598a3eacec669a20f26881a9ccd10dbd5c4476
Yonas Negash <yonas.negash91@gmail.com>
2019-08-30 17:02:23 +0300

    add paginator

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from contribution.models import Contribution
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def index(request):
     contribution = Contribution.objects.all()
     
+    paginator = Paginator(contribution, 1)
+    page = request.GET.get('page')
+    paged_contributions = paginator.get_page(page)
+
     context = {
-        'contributions': contribution
+        'contributions': paged_contributions
     }
 
     return render(request, 'pages/index.html', context)
